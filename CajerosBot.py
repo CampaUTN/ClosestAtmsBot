27a269b470a0bb59f6b5d3699f27200e573a7a40
CampaUTN <CampaUTN@users.noreply.github.com>
2018-05-02 05:04:41 -0300

    Listo 3 y comienzo mapa

--- CajerosBot.py
+++ CajerosBot.py
@@ -1,56 +1,82 @@
 import telegram
 from telegram.ext import *
-import logging, requests, csv, json
+import requests, csv, json, geopy.distance, geocoder
 
 token = '567792160:AAHHkjURiYh5s2GSQm-YzMq7tVdFf-7PLJo'
-
-jsonfile = open('file.json', 'w')
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
+list_dic = []
 
 def start(bot, update):
-    update.message.reply_text('Que rica estaba la torta!!')
+    update.message.reply_text("asd")
 
+def link(bot, update):
+    cajerosLink = select3Nearest("LINK")
+    update.message.reply_text(armarStringPantalla(cajerosLink))
+    devolverMapa(cajerosLink,bot,update)
 
-def echo(bot, update):
-    update.message.reply_text(update.message.text)
+def banelco(bot, update):
+    cajerosLink = select3Nearest("BANELCO")
+    update.message.reply_text(armarStringPantalla(cajerosLink))
+
+#40.702147,-74.015794
+def devolverMapa(cajeros,bot,update):
+    c1 = cajeros[0]["LAT"].replace(",",".") + "," + cajeros[0]["LNG"].replace(",",".")
+    c2 = cajeros[1]["LAT"].replace(",",".") + "," + cajeros[1]["LNG"].replace(",",".")
+    c3 = cajeros[2]["LAT"].replace(",",".") + "," + cajeros[2]["LNG"].replace(",",".")
+
+    url = '''https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:1%7C{cord1}&markers=color:green%7Clabel:2%7C{cord2}&markers=color:red%7Clabel:3%7C{cord3}
+            &key=AIzaSyDtFJmOZiEyqegfXU2gY_A2AlrFSCl9C2c'''.format(cord1=c1,cord2=c2,cord3=c3)
+
+    r = requests.get(url)
+    bot.send_photo(chat_id=update.chat_id, photo=url)
+
+def armarStringPantalla(cajeros):
+    ret = []
+    for idx, c in enumerate(cajeros):   
+        line = "{id}. {banco} - {dir} \n".format(id=str(idx+1),banco=c["BANCO"],dir=c["DOM_ORIG"])
+        ret.append(line)
+    return ''.join(ret)
+
+def getLink():
+    print(select3Nearest("LINK"))
+
+def getBanelco():
+    print(select3Nearest("BANELCO"))
+
+def select3Nearest(red):
+    return sorted(calcularDistancias(filtrarRed(red)), key=lambda k: k["DISTANCE"])[:3]
+
+def filtrarRed(tipo):
+    return list(filter(lambda d: d["RED"]==tipo,list_dic))
+
+def calcularDistancias(lista):
+    myPos = geocoder.ip("me").latlng
+    for cajero in lista:
+        posCajero = (float(cajero["LAT"].replace(",",".")), float(cajero["LNG"].replace(",",".") ))
+        cajero["DISTANCE"] = geopy.distance.vincenty(myPos, posCajero).km
+    return lista
 
 def main():
-    # Manejador de eventos 
     updater = Updater(token)
-
     dp = updater.dispatcher
-
-    # Reconocer los siguientes eventos
+    dp.add_handler(CommandHandler("link", link))
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # Start
+    dp.add_handler(CommandHandler("banelco", banelco))
     updater.start_polling()
+
+    #getCsv()
+    csvToDic()
+
     updater.idle()
 
-    getCsv()
-
+def getCsv():
+    r = requests.get("https://data.buenosaires.gob.ar/api/files/cajeros-automaticos.csv/download/csv") 
+    with open("cajeros.csv",'wb') as f:
+        f.write(r.content)
+    
+def csvToDic():
+    reader = csv.DictReader(open('cajeros.csv', 'r'),delimiter=";")
+    for line in reader:
+        list_dic.append(line)
 
 if __name__ == '__main__':
     main()
-
-def getCsv():
-    url = "https://data.buenosaires.gob.ar/api/files/cajeros-automaticos.csv/download/csv"
-    r = requests.get(url, allow_redirects=True)
-    csvToJson(r.content)
-    #open('cajerosCsv.csv', 'wb').write(r.content)
-
-def csvToJson(csvFile):
-    fieldnames = ('ID','LAT','LNG','BANCO','RED','DOM_GEO','TERMINALES','WEB','ACTUALIZAC','DOM_NORMA','BARRIO','COMUNA','CODIGO_POSTAL','CODIGO_POSTAL_ARGENTINO')
-    reader = csv.DictReader( csvFile, fieldnames)
-    for row in reader:
-        json.dump(row, jsonfile)
-        jsonfile.write('\n')
